9bb49f0b3fe07326936a7b01e0503a7488bc68e6
Irina Azizova <devazizova@mail.ru>
2023-04-03 10:12:00 +0300

    feat: layout adaptive pets-galery section

--- shelter/assets/js/index.js
+++ shelter/assets/js/index.js
@@ -3,10 +3,22 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const burgerBtn = document.querySelector('.burger-menu'),
-          navMenu = document.querySelector('.nav');
+          burgerLines = burgerBtn.querySelectorAll('.burger-menu__line'),
+          navMenu = document.querySelector('.nav'),
+          navlinks = navMenu.querySelectorAll('.nav__link');
 
     burgerBtn.addEventListener('click', () => {
             burgerBtn.classList.toggle('burger-menu_active');
             navMenu.classList.toggle('nav_active');
+            burgerLines.forEach(line => {
+                line.classList.toggle('burger-menu__line_active')
+            });
     });
+
+    navlinks.forEach(eachLink => {
+        eachLink.addEventListener('click', () => {
+            navMenu.classList.remove('nav_active');
+            burgerBtn.classList.remove('burger-menu_active');
+        });
+    })
 });
