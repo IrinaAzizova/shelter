31c0572cea12ef9a0e5c5b3db97976a37509614c
Irina Azizova <devazizova@mail.ru>
2023-04-10 10:27:51 +0300

    feat: layout adaptive modal window

--- shelter/assets/js/index.js
+++ shelter/assets/js/index.js
@@ -115,17 +115,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	let totalStides = 3,
 		currentSlide = 0,
-		cardsOnSlide = 3;
+		cardsOnSlide = 3,
+		currentCardsIndex = [],
+		doneResizing;
+
 	
-	if (+window.getComputedStyle(document.documentElement).width.slice(0, -3) < 768) {
-		console.log(+window.getComputedStyle(document.documentElement).width.slice(0, -3));
-		cardsOnSlide = 1;
-		totalStides = 8;
-	} else if (+window.getComputedStyle(document.documentElement).width.slice(0, -3) < 768) {
-		console.log(+window.getComputedStyle(document.documentElement).width.slice(0, -3));
-		cardsOnSlide = 2;
-		totalStides = 4;
+	const toListenerResize = () => {
+		if (parseFloat(window.getComputedStyle(document.documentElement).width) < 768) {
+			cardsOnSlide = 1;
+			totalStides = 8;
+		} else if (parseFloat(window.getComputedStyle(document.documentElement).width) < 1280) {
+			cardsOnSlide = 2;
+			totalStides = 4;
+		} else {
+			totalStides = 3;
+			cardsOnSlide = 3;
+		}
 	}
+	toListenerResize();
 
 	const toCreateCard = ({img, petName}, animateClass) => {
 		const card = document.createElement('div');
@@ -138,35 +145,133 @@ document.addEventListener('DOMContentLoaded', () => {
 		slidesContainer.append(card);
 	}
 
-	const toCreateSlide = (animateClass) => {
+	const toCalcRandomInt = (minInt, maxInt) => {
+		let random = minInt + Math.random() * (maxInt + 1 - minInt);
+		return Math.floor(random);
+	}
+
+	const toCalculateCardsIndex = () => {
+		let newSlidesIndex = [];
+
+		const toCreateArr = () => {
+			for (let i = 0; i < cardsOnSlide; i++) {
+				let randInt = toCalcRandomInt(0, 7);
+				newSlidesIndex[i] = randInt;
+				while (newSlidesIndex.indexOf(randInt) != i) {
+					randInt = toCalcRandomInt(0, 7);
+					newSlidesIndex[i] = randInt;
+				}
+			}
+			return newSlidesIndex;
+		}
+
+		toCreateArr();
+
+		for (let k = 0; k < cardsOnSlide; k++) {
+			for (let j = 0; j < cardsOnSlide; j++) {
+				while (currentCardsIndex[k] === newSlidesIndex[j]) {
+					toCreateArr();
+					j = 0; k = 0;
+				}
+			}
+		}
+		currentCardsIndex = newSlidesIndex;
+		return newSlidesIndex;
+	}
+	toCalculateCardsIndex();
+
+    //---------forming cards in order---------
+	/* const toCreateSlide = (animateClass) => {
 		slidesContainer.innerHTML = '';
 		for (let i = 0; i < cardsOnSlide; i++) {
 			if (petsCatalogue[i + currentSlide * cardsOnSlide]) {				
 				toCreateCard(petsCatalogue[i + currentSlide * cardsOnSlide], animateClass);
 			}
 		}
+	} */
+	const toCreateSlide = (animateClass) => {
+		slidesContainer.innerHTML = '';
+		toCalculateCardsIndex().forEach( item => {
+			toCreateCard(petsCatalogue[item], animateClass);
+		});
 	}
-
 	toCreateSlide('animate__slideInRight');
 
 	nextBtn.addEventListener('click', () => {
 		currentSlide = currentSlide == totalStides - 1 ? 0 : currentSlide + 1;
-		console.log(currentSlide);
 		toCreateSlide('animate__slideInRight');
 	});
 
 	prevBtn.addEventListener('click', () => {
 		currentSlide = currentSlide == 0 ? totalStides - 1 : currentSlide - 1;
-		
-		console.log(currentSlide);
 		toCreateSlide('animate__slideInLeft');
 	}); 
 
-	/*console.log(+window.getComputedStyle(document.documentElement).width.slice(0, -3));
+	window.addEventListener('resize', function(){
+        this.clearTimeout(doneResizing);
+        doneResizing = this.setTimeout(function(){
+            toListenerResize();
+			toCreateSlide('animate__slideInRight');
+        }, 500);
+    });	
 
-	if (+window.getComputedStyle(document.documentElement).width.slice(0, -3) < 1279) {
-		console.log(+window.getComputedStyle(document.documentElement).width.slice(0, -3));
-		cardsOnSlide = 2;
-		slides = 4;
-	} */
+
+
+	/*----------Popap----------*/
+
+	const modalOverlay = document.querySelector('.overlay_modal'),
+		  closeBtn = modalOverlay.querySelector('.modal__close');
+
+	let petName = '',
+		petCard;
+
+	slidesContainer.addEventListener('click', (event) => {
+		if (event.target.matches('.pet-card') || event.target.parentNode.matches('.pet-card')) {
+			petCard = event.target.matches('.pet-card') ? event.target : event.target.parentNode;
+			petName = petCard.querySelector('.pet-card__title').textContent;
+
+			petsCatalogue.forEach( item => {
+				if (item.petName === petName) {
+					modalOverlay.innerHTML = `
+						<div class="modal-window">
+							<button class="btn btn_secondary modal__close" data-modal="close">
+								<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path fill-rule="evenodd" clip-rule="evenodd" d="M7.42618 6.00003L11.7046 1.72158C12.0985 1.32775 12.0985 0.689213 11.7046 0.295433C11.3108 -0.0984027 10.6723 -0.0984027 10.2785 0.295433L5.99998 4.57394L1.72148 0.295377C1.32765 -0.098459 0.68917 -0.098459 0.295334 0.295377C-0.0984448 0.689213 -0.0984448 1.32775 0.295334 1.72153L4.57383 5.99997L0.295334 10.2785C-0.0984448 10.6723 -0.0984448 11.3108 0.295334 11.7046C0.68917 12.0985 1.32765 12.0985 1.72148 11.7046L5.99998 7.42612L10.2785 11.7046C10.6723 12.0985 11.3108 12.0985 11.7046 11.7046C12.0985 11.3108 12.0985 10.6723 11.7046 10.2785L7.42618 6.00003Z" fill="#292929"/>
+									</svg>
+							</button>
+							<img src="${item.img}" alt="${item.petName}" class="modal__img">
+							<div class="modal__content">
+								<p class="modal__name heading">${item.petName}</p>
+								<p class="modal__breed">${item.species}</p>
+								<p class="modal__descr">${item.description}</p>
+								<ul class="modal__list">
+									<li class="modal__item">Age: <span>2 months</span></li>
+									<li class="modal__item">Inoculations: <span>none</span></li>
+									<li class="modal__item">Diseases: <span>none</span></li>
+									<li class="modal__item">Parasites: <span>none</span></li>
+								</ul>
+							</div>
+						</div>
+					`;
+					console.log(item);
+				}
+			});
+			console.log(petName);
+
+			modalOverlay.style.display = 'flex';
+			document.body.style.overflow = "hidden";
+		}
+	});
+
+	closeBtn.addEventListener('click', () => {		
+		modalOverlay.style.display = 'none';
+		document.body.style.overflow = "visible";
+	});
+
+	modalOverlay.addEventListener('click', (event) => {
+		if (event.target.matches('.overlay_modal') || event.target.matches('.modal__close')) {
+			modalOverlay.style.display = 'none';
+			document.body.style.overflow = "visible";
+		}
+	});
 });
